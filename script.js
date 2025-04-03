620a10c8e7ce2baef092fac5419b1fbf722b71ea
Mrguccu <mrguccu@gmail.com>
2025-04-03 18:22:26 -0500

    fixed sidebar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showSidebar(){
-    
+    const sidebar = document.querySelector('.mobile-navbar');
+    sidebar.style.display = 'block';
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.mobile-navbar');
+    sidebar.style.display = 'none';
 }
